b8cafa7084c2aaec54ac92366b7d6b07163aef28
Vladadon <v.golubin2017@yandex.ru>
2023-11-13 09:05:24 +0300

    Some work

--- HomeWork_09.11/Class1.cs
+++ HomeWork_09.11/Class1.cs
@@ -16,6 +16,41 @@ namespace HomeWork_09._11
             XmlDocument xmlDoc = new XmlDocument();
             xmlDoc.Load("C:\\Users\\1\\source\\repos\\HomeWork_09.11\\HomeWork_09.11\\people.xml");
             XmlElement? xRoot = xmlDoc.DocumentElement;
+
+            XmlElement personElem = xmlDoc.CreateElement("person");
+
+            XmlAttribute nameAttr = xmlDoc.CreateAttribute("name");
+
+            XmlElement sizeElem = xmlDoc.CreateElement("size");
+            XmlElement formeElem = xmlDoc.CreateElement("forme");
+            XmlElement tasteElem = xmlDoc.CreateElement("taste");
+            XmlElement fillingElem = xmlDoc.CreateElement("filling");
+            XmlElement cookMethodElem = xmlDoc.CreateElement("cookMethod");
+
+            XmlText nameText = xmlDoc.CreateTextNode("с маком");
+            XmlText sizeText = xmlDoc.CreateTextNode("Средняя");
+            XmlText formeText = xmlDoc.CreateTextNode("Прямоугольная");
+            XmlText tasteText = xmlDoc.CreateTextNode("9");
+            XmlText fillingText = xmlDoc.CreateTextNode("Да");
+            XmlText cookMethodText = xmlDoc.CreateTextNode("Испечённая");
+
+            nameAttr.AppendChild(nameText);
+            sizeElem.AppendChild(sizeText);
+            formeElem.AppendChild(formeText);
+            tasteElem.AppendChild(tasteText);
+            fillingElem.AppendChild(fillingText);
+            cookMethodElem.AppendChild(cookMethodText);
+
+            personElem.Attributes.Append(nameAttr);
+
+            personElem.AppendChild(sizeElem);
+            personElem.AppendChild(formeElem);
+            personElem.AppendChild(tasteElem);
+            personElem.AppendChild(fillingElem);
+            personElem.AppendChild(cookMethodElem);
+
+            xRoot?.AppendChild(personElem);
+
             if (xRoot != null)
             {
                 foreach (XmlElement xnode in xRoot)
